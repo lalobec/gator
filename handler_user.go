9a04141208b4915f6718c3d81f61ab63915ce526
lalobgc <lalobg.c@gmail.com>
2025-09-04 15:40:48 -0600

    register command working, and login command improved

--- handler_user.go
+++ handler_user.go
@@ -1,20 +1,75 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"github.com/google/uuid"
+	"github.com/lalobec/gator/internal/database"
+	"time"
 )
 
+import _ "github.com/lib/pq"
+
 func handlerLogin(s *state, cmd command) error {
-	if len(cmd.arguments) == 0 {
-		return fmt.Errorf("You must provide a username")
+	if len(cmd.arguments) != 1 {
+		return fmt.Errorf("usage: %v <name>", cmd.name)
 	}
 	name := cmd.arguments[0]
 
-	err := s.cfg.SetUser(name)
+	ctx := context.Background()
+	_, err := s.db.GetUser(ctx, name)
+	if err == sql.ErrNoRows {
+		return fmt.Errorf("%s user does not exist", name)
+	}
+
+	err = s.cfg.SetUser(name)
 	if err != nil {
 		return fmt.Errorf("An error ocurred while login: %v\n", err)
 	}
-	
+
 	fmt.Printf("%s is logged in the system \n", cmd.arguments[0])
 	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.arguments) != 1 {
+		return fmt.Errorf("usage: %v <name>", cmd.name)
+	}
+
+	name := cmd.arguments[0]
+
+	args := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+	}
+
+	ctx := context.Background()
+
+	_, err := s.db.GetUser(ctx, args.Name)
+	if err == sql.ErrNoRows {
+		user, errCreate := s.db.CreateUser(ctx, args)
+		if errCreate != nil {
+			return fmt.Errorf("Could not create user: %w\n", errCreate)
+		}
+		err2 := s.cfg.SetUser(args.Name)
+		if err2 != nil {
+			return fmt.Errorf("Could not set current user: %w\n", err2)
+		}
+		fmt.Println("User created successfully")
+		printUser(user)
+		return nil
+	} else if err != nil {
+		return err
+	} else {
+		return fmt.Errorf("username %v already exists \n", args.Name)
+	}
+
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:			%v\n", user.ID)
+	fmt.Printf(" * Name:			%v\n", user.Name)
 }
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/lalobec/gator/internal/config"
+	"github.com/lalobec/gator/internal/database"
 	"log"
 	"os"
+	_ "github.com/lib/pq"
 )
 
+
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -16,33 +21,41 @@ func main() {
 		log.Fatalf("Error reading config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v\n", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
 	programState := &state{
+		db: dbQueries,
 		cfg: &cfg,
 	}
 
-	cmds := commands {
-		registeredCommands : make(map[string]func(*state, command) error),
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
-	
+	cmds.register("register", handlerRegister)
+
 	if len(os.Args) < 2 {
 		log.Fatal("usage: cli <command> [args...]")
 	}
 
-	typed_cmd := command {
-		name: os.Args[1],
+	typed_cmd := command{
+		name:      os.Args[1],
 		arguments: os.Args[2:],
 	}
 
 	err = cmds.run(programState, typed_cmd)
 	if err != nil {
-		log.Fatalf("Something bad happened running the command, %v\n", err)
+		log.Fatal(err)
 	}
 }
 
-
-	/*
+/*
 	if err := cfg.SetUser("lalobg"); err != nil {
 		log.Fatalf("Could not set user: %v", err)
 	}
-	*/
+*/
